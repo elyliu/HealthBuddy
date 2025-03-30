d039ad419492867174bb39efc5b33cc1db2cc5a4
Ely Liu <elyliu@gmail.com>
2025-03-29 18:31:49 -0700

    Update server URL to correct Render URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  serverUrl: process.env.REACT_APP_SERVER_URL || 'http://192.168.68.56:5001'
+  serverUrl: process.env.REACT_APP_SERVER_URL || 'https://healthbuddy-uz19.onrender.com'
 };
 
 export default config; 
